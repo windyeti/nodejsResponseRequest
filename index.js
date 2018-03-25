c6d8e3ae86039b77ef076dc34b37c0c65385dc58
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2018-03-26 00:00:42 +0300

    connect to tproger.ru

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const request = require('request');
 const cheerio = require('cheerio');
-
-request('http://vetratoria.ru/ru', (err, response, body) => {
+//
+request('https://tproger.ru/', (err, response, body) => {
     if(err) throw err;
     if(response.statusCode === 200) {
         const $$ = cheerio.load(body);
-        console.log('длина', $$('h3'));
-        $$('h3').each( (ind, item) => console.log( `${$$(item).text()}` ));
+        console.log('длина', $$('.news-title').length);
+        $$('.news-title').each( (ind, item) => console.log( `${$$(item).text()}` ));
     }
 });

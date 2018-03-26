c9e3626ca495ac8be982a9e2eb641f288e51bfb2
Егор Тиханин <egortihanin@Egors-Mac-Pro.local>
2018-03-26 18:26:30 +0300

    from job machine

--- index.js
+++ index.js
@@ -1,31 +1,33 @@
-const request = require('request');
-const cheerio = require('cheerio');
+// const request = require('request');
+// const cheerio = require('cheerio');
 const express = require('express');
 const consolidate = require('consolidate');
+// const handlebars = require('handlebars');
+const handlebars = require('handlebars-engine');
 
 const app = express();
 
 // middlewave
-app.use((req, res, next) => {
-    req.titles = {
-        head : 'Заголовки с сайта Pproger.ru',
-        titles : ['Первый заголовок', 'Второй заголовок']
-    };
-    next();
-});
-app.get('/', (req, res) => {
-   res.send(`${req.titles.head}`);
-});
-app.get('/titles', (req, res) => {
-
-});
-// request('https://tproger.ru/', (err, response, body) => {
-//     if(err) throw err;
-//     if(response.statusCode === 200) {
-//         const $$ = cheerio.load(body);
-//         // console.log('длина', $$('.news-title').length);
-//         // $$('.news-title').each( (ind, item) => console.log( `${$$(item).text()}` ));
-//         $$('.news-title').each( (ind, item) => (`${$$(item).text()}` ));
-//     }
+// app.use((req, res, next) => {
+//     req.titles = {
+//         head : 'Заголовки с сайта Pproger.ru',
+//         news : ['Первый заголовок', 'Второй заголовок']
+//     };
+//     next();
 // });
+
+app.engine('hbs', consolidate.handlebars);
+
+// установка по-умолчанию для шаблонизатора
+app.set('view engine', 'hbs');
+// имя шаблона в проекте и путь к его файлу
+app.set('view', './view');
+
+app.get('/', (req, res) => {
+    res.render('view', {
+        head : 'Привет, Сергей1',
+        news : 'Привет, Сергей2'
+    });
+});
+
 app.listen(8888);
